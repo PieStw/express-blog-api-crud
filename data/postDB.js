9d3004b2c606180818d4c82f4becbae0a2c471ea
PieStw <piero.stalteri@icloud.com>
2024-11-21 13:52:25 +0100

    creazione file data e controller

---
+++ data/postDB.js
@@ -0,0 +1,9 @@
+const posts = [
+    {id: 0, titolo: "Post", immagine: "img/ciambellone.jpeg", contenuto: "Lorem ipsum", tags: ["sole", "mare"]},
+    {id: 1, titolo: "Post1", immagine: "img/cracker_barbabietola.jpeg", contenuto: "Lorem ipsum", tags: ["mare"]},
+    {id: 2, titolo: "Post2", immagine: "img/pane_fritto_dolce.jpeg", contenuto: "Lorem ipsum", tags: ["radio"]},
+    {id: 3, titolo: "Post3", immagine: "img/pasta_barbabietola.jpeg", contenuto: "Lorem ipsum", tags: ["vip"]},
+    {id: 4, titolo: "Post4", immagine: "img/torta_paesana.jpeg", contenuto: "Lorem ipsum", tags: ["tv"]},
+];
+
+module.exports = posts;
